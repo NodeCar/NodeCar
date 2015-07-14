0a6b8d99b9be9d9c9e1bec2499253c2f56e0825d
Hongcai Deng <admin@dhchouse.com>
2015-07-14 22:16:56 +0800

    test

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# NodeCar
+NodeCar
+=======
+
+Preparation
+-------------
+
+### Hardware
+- Raspberry Pi (A, B, B+ or 2)
+- 0.95 inch SSD1331 RGB OLED
+
+### Software
+- Any Linux distro which use Kernel compiled with spi, i2c and fbtft modules. Raspbian is recommanded.
+- Nodes.js 0.12.x
--- test/ultrasonic.js
+++ test/ultrasonic.js
@@ -1,18 +1,12 @@
-var serialport = require('serialport');
-var SerialPort = require("serialport").SerialPort;
-var serialPort = new SerialPort("/dev/ttyAMA0", {
-  baudrate: 9600,
-  parser: serialport.parsers.raw
-});
-serialPort.on("open", function () {
-  console.log('open');
-  serialPort.on('data', function(data) {
-    console.log('data received');
-    console.log(data.readUInt16BE(0)/10);  
-  });
-  setInterval(function(){
-    var data = new Buffer(1);
-    data.writeUInt8(0x55, 0);
-    serialPort.write(data);
-  }, 500);
+var i2c = require('i2c');
+var address = 8;
+var wire = new i2c(address, {device: '/dev/i2c-1'});
+
+wire.scan(function(err, data) {
+  if(err) {
+    console.error(err);
+  }
+  else {
+    console.log(data);
+  }
 });
