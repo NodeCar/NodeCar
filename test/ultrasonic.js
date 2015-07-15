36204fef477eb9409c3a36fc706e0141d58ac803
Hongcai Deng <admin@dhchouse.com>
2015-07-15 16:07:12 +0000

    add ultrasonic test

--- test/ultrasonic.js
+++ test/ultrasonic.js
@@ -1,4 +1,5 @@
 var i2c = require('i2c');
+var util = require('util');
 var address = 8;
 var wire = new i2c(address, {device: '/dev/i2c-1'});
 
@@ -11,11 +12,12 @@ wire.scan(function(err, data) {
   }
 });
 
-wire.readByte(function(err, res) {
+wire.readBytes(0, 2, function(err, res) {
   if(err) {
     console.error(err);
   }
   else {
     console.log(res);
+    console.log(res.readUInt16BE(0));
   }
 });
