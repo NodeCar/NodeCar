0800db84eceaef11bbd09410d93e92d83e45f4fc
Hongcai Deng <admin@dhchouse.com>
2015-09-27 20:05:50 +0800

    update

--- test/framebuffer.js
+++ test/framebuffer.js
@@ -6,10 +6,14 @@
 
 var pngtolcd = require('png-to-lcd');
 var Framebuffer = require('framebuffer');
+var mmap = require('mmap-io');
 
 var fb = new Framebuffer('/dev/fb0');
 console.log(fb.toString());
 
-pngtolcd(__dirname + '/../resource/logo.png', function(err, buffer) {
-    fb.fb = buffer;
+pngtolcd(__dirname + '/../resource/logo.png', function (err, buffer) {
+    if (err) console.log(err);
+    else {
+        fb.fbp.write(buffer);
+    }
 });
